b131563c6cff896216105bf87c5d04ca62647dc7
filipsuvajac <filipsuvajac906@gmail.com>
2025-12-09 20:28:48 +0100

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="flex justify-center">
+      <p className="font-bold text-blue-500">Najboljsi RPO projekt v zgodovini </p>
+    </div>
+  )
+}
+
+export default App;
