7d33c54d9d75a45c411a74151720fd87975e555e
Luka Manfreda <luka.manfreda@gmail.com>
2025-12-18 19:25:04 +0100

    dodan center block v Login.tsx

---
+++ src/pages/Login.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Login() {
+  return (
+    <div className="flex justify-center items-center h-screen bg-gray-100">
+      {/* center block */}
+      <div className="bg-white p-8 rounded-xl shadow-lg w-[400px] h-[400px]"></div>
+    </div>
+  );
+}
+
+export default Login;
