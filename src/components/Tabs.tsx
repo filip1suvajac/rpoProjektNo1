630ef97913b4932262353a209a7b0e8addd5b8a7
Anastasija <anastasija.lukarova@student.um.si>
2025-12-21 21:24:51 +0100

    Prevedeno besedilo

--- src/components/Tabs.tsx
+++ src/components/Tabs.tsx
@@ -8,23 +8,23 @@ type TabsProps = {
 };
 
 const Tabs: React.FC<TabsProps> = ({ activeTab, onChange }) => (
-  <div className="inline-flex bg-slate-900 rounded-full p-1"> {/* malo veći padding okvira */}
+  <div className="inline-flex bg-slate-900 rounded-full p-1">
+    {}
     <button
       onClick={() => onChange('casino')}
       className={`px-4 py-2 rounded-full text-white text-base font-semibold transition ${
         activeTab === 'casino' ? 'bg-slate-600' : 'hover:bg-slate-600'
       }`}
     >
-      Casino Bets
+      Igralne stavke
     </button>
-
     <button
       onClick={() => onChange('leaderboard')}
       className={`px-4 py-2 rounded-full text-white text-base font-semibold transition ${
         activeTab === 'leaderboard' ? 'bg-slate-600' : 'hover:bg-slate-600'
       }`}
     >
-      Leaderboard
+      Lestvica najboljših
     </button>
   </div>
 );
