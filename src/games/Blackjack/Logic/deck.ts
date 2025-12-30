42d23fd81c176dd11184306231201cf7dad2571d
Luka Manfreda <luka.manfreda@gmail.com>
2025-12-30 12:42:51 +0100

    dodana funkcija za ustvarjanje decka

--- src/games/Blackjack/Logic/deck.ts
+++ src/games/Blackjack/Logic/deck.ts
@@ -10,3 +10,22 @@ export interface Card{
 export interface Deck{
     cards: Card[];
 }
+
+export function createDeck(): Deck{
+    const suits: Suit[] = ['hearts', 'diamonds', 'clubs', 'spades'];
+    const ranks: Rank[] = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K'];
+
+    const cards: Card[] = [];
+  
+  suits.forEach(suit => {
+    ranks.forEach(rank => {
+      cards.push({
+        suit,
+        rank,
+        id: `${rank}-${suit}-${Math.random()}`
+      });
+    });
+  });
+  
+  return { cards };
+}
