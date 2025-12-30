0cceaeab0e5463b76169578511eb5d0c2accce50
Luka Manfreda <luka.manfreda@gmail.com>
2025-12-30 11:50:05 +0100

    Dodan Blackjack.tsx v pages

---
+++ src/pages/Blackjack.tsx
@@ -0,0 +1,9 @@
+function Blackjack() {
+  return (
+    <div className="min-h-screen bg-gray-900 p-4">
+      <h1 className="text-white text-3xl">Blackjack</h1>
+    </div>
+  );
+}
+
+export default Blackjack;
