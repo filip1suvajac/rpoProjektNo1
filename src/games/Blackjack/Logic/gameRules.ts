8b64574165ee36c0b03bc68c3ac0713bd7d390ce
Luka Manfreda <luka.manfreda@gmail.com>
2025-12-30 14:20:12 +0100

    dodana funkcija za izracun vrednosti roke

--- src/games/Blackjack/Logic/gameRules.ts
+++ src/games/Blackjack/Logic/gameRules.ts
@@ -5,3 +5,22 @@ export function getCardValue(rank: Rank): number {
     if(rank === 'J' || rank === 'Q' || rank === 'K') return 10;
     return parseInt(rank);
 }
+
+export function calculateHandValue(cards: Card[]): number{
+    let total = 0;
+    let aces = 0;
+
+    cards.forEach(card=> {
+        const value = getCardValue(card.rank);
+        total += value;
+        if(card.rank === 'A') aces++;
+    });
+
+    while(total > 21 && aces > 0){
+        total -= 10;
+        aces--;
+    }
+
+    return total;
+}
+
