7e5a7146250f6c4c53403dcbef32b33d76a96aff
Matija Dukaric <matijadukaric1@gmail.com>
2026-01-02 23:56:33 +0100

    Naredil sem file Chicken-BetPanel in file Chicken-StatisticsPanel ki sta namenjena implamentacijah ostalih sosolcev

---
+++ src/components/Chicken-BetPanel.tsx
@@ -0,0 +1,4 @@
+
+{/* Oseba 3: Prikaz trenutnega stanja (balance) */}
+{/* Sem dodaj vse stvari kot smo se zmenili na sestanku.*/}
+
---
+++ src/components/Chicken-StatisticsPanel.tsx
@@ -0,0 +1,2 @@
+{/* Oseba 4 */}
+{/* Naredi tako kot smo se zmenili na sestanku*/}
