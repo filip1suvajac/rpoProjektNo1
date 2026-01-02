363d6dca5130b0f3e90e8ef0a69b6a7aa43c7f5d
Matija Dukaric <matijadukaric1@gmail.com>
2026-01-02 23:51:58 +0100

    Naredil sem file Chicken-Roasted ki prikazuje kaj se prikaze ko uporabnik izgubi

---
+++ src/components/Chicken-Roasted.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const ChickenRoasted: React.FC = () => (
+  <div className="text-6xl animate-spin-slow drop-shadow-2xl">
+    🍗
+  </div>
+);
+
+export default ChickenRoasted;
